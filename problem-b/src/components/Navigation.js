cb7458abf3ff6770829b25f2a731ff5c169c5f4d
carsonfk <carson.f.k@outlook.com>
2024-02-22 04:19:22 -0800

    update problem-b

--- problem-b/src/components/Navigation.js
+++ problem-b/src/components/Navigation.js
@@ -1,1 +1,29 @@
 import React from 'react';
+
+export function AboutNav() {
+    return (
+        <nav id="aboutLinks">
+          <h2>About</h2>
+          <ul className="list-unstyled">
+            <li><a href="#/">How to Adopt</a></li>
+            <li><a href="#/">Volunteering</a></li>
+            <li><a href="#/">Events</a></li>
+            <li><a href="#/">Donate</a></li>
+            <li><a href="#/">About Us</a></li>
+          </ul>
+        </nav>
+    );
+}
+
+export function BreedNav({breeds}) {
+    return (
+        <nav id="breedLinks">
+          <h2>Pick a Breed</h2>
+          <ul className="list-unstyled">
+            {breeds.map(temp => {
+                return <li key={temp}><a href="">{temp}</a></li>
+            })}
+          </ul>
+        </nav>
+    );
+}
--- problem-b/src/components/PetList.js
+++ problem-b/src/components/PetList.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+export function PetCard({petData}) {
+    return (
+        <div className="card">
+            <img className="card-img-top" src="img/069b8257-f9db-4034-908f-12b8cea76693.jpg" alt={petData.name} />
+            <div className="card-body">
+                <h3 className="card-title">{petData.name}</h3>
+                <p className="card-text">{petData.sex + " " + petData.breed}</p>
+            </div>
+        </div>
+    );
+}
