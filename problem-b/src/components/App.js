983996b4e276bb8f60e5713e3437042665853df3
carsonfk <carson.f.k@outlook.com>
2024-02-22 19:29:15 -0800

    finish problem-b

--- problem-b/src/components/App.js
+++ problem-b/src/components/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AboutNav } from './Navigation';
 import { BreedNav } from './Navigation';
 import _ from 'lodash';
+import PetList from './PetList';
 
 function App(props) {
+  const [pets, setPets] = useState(props.pets);
+  //console.log(pets)
+  function adoptPet(name) {
+    let newState = pets.map((temp) => { 
+      let copy = {...temp};  
+      //console.log(temp.name)  
+      //console.log(name)  
+      if (copy.name == name) {
+        copy.adopted = true;
+        //console.log(copy);
+        return copy;
+      } else {
+        return copy;
+      }
+    });
+    setPets(newState);
+  }
   return (
     <body>
       <header className="jumbotron jumbotron-fluid py-4">
@@ -15,12 +33,12 @@ function App(props) {
       <main className="container">
         <div className="row">
           <div id="navs" className="col-3">
-            <BreedNav breeds={Object.keys(_.groupBy(props.pets, 'breed'))} />
+            <BreedNav breeds={Object.keys(_.groupBy(pets, 'breed'))} />
             <AboutNav />
           </div> 
 
           <div id="petList" className="col-9">
-          
+            <PetList pets={pets} adoptCallback={adoptPet} />
           </div>
         </div>
       </main>
--- problem-b/src/components/PetList.js
+++ problem-b/src/components/PetList.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
-export function PetCard({petData}) {
+export function PetCard({petData, adoptCallback}) {
+    let h = petData.name;
+    //console.log(1)
+    let displayedName = petData.name;
+    if (petData.adopted == true) {
+        displayedName += " (Adopted)";
+    }
     return (
-        <div className="card">
-            <img className="card-img-top" src="img/069b8257-f9db-4034-908f-12b8cea76693.jpg" alt={petData.name} />
+        <div className="card" onClick={() => adoptCallback(petData.name)}>
+            <img className="card-img-top" src={petData.img} alt={petData.name} />
             <div className="card-body">
-                <h3 className="card-title">{petData.name}</h3>
+                <h3 className="card-title">{displayedName}</h3>
                 <p className="card-text">{petData.sex + " " + petData.breed}</p>
             </div>
+        </div>
+    );
+}
+
+export default function PetList({pets, adoptCallback}) {
+    return (
+        <div>
+            <h2>Dogs for Adoption</h2>
+        
+    
+            <div className="card-deck">
+                {pets.map(temp => {
+                    return <PetCard key = {temp.name} adoptCallback={adoptCallback} petData={temp} />         
+                })}
+            </div> 
         </div>
     );
 }
